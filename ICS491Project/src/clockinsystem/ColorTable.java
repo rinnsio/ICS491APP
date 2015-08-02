c1140ec98a81723b4747dff6d141258202f103c2
MandLRPG <knaka@hawaii.edu>
2015-08-02 12:23:01 -1000

    Changed package name

--- ICS491Project/src/clockinsystem/ColorTable.java
+++ ICS491Project/src/clockinsystem/ColorTable.java
@@ -1,4 +1,4 @@
-package gui;
+package clockinsystem;
 
 
 import java.awt.Color;
--- ICS491Project/src/clockinsystem/logs.java
+++ ICS491Project/src/clockinsystem/logs.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gui;
+package clockinsystem;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
